50de5e9579a439f9690ebc79a93905d9cc013240
madhu <madhumithar2012@gmail.com>
2022-08-10 12:54:10 +0530

    Added Resorts

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
     <div className="App">
     <Navbar />
-    <Routes basename='/home'>
+    <Routes>
     <Route path="/" element={<Navigate to="/home" />} />
     <Route path="/home" element={<Home />} />
     <Route path='/about' element={<AboutUs/>}/>
